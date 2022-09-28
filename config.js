b134c549ee9d24bf793e7d5dc632eee3793c2fe4
H02c4n <svtr.hozcan@gmail.com>
2022-09-28 11:01:00 +0200

    change app.js to index.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDS9uHnCCpBhlwksx_HMUw7e_Eq2Lj6B8E",
-  authDomain: "teamwork-870ef.firebaseapp.com",
-  projectId: "teamwork-870ef",
-  storageBucket: "teamwork-870ef.appspot.com",
-  messagingSenderId: "938967222340",
-  appId: "1:938967222340:web:30737ff7f8ee709f06d411",
-  measurementId: "G-M9FQ4NQVVQ",
-};
